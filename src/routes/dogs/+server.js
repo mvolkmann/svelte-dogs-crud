83ce0c392d59c287953be8731ac1fe4a0d06acc7
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-03-26 10:09:22 -0500

    progress

---
+++ src/routes/dogs/+server.js
@@ -0,0 +1,8 @@
+export function GET() {
+  const number = Math.floor(Math.random() * 6) + 1;
+  return new Response(number, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}
